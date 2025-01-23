0748895730f2749690aff3a3e70ec5f59281e02f
John Doe <lukeschneider7@yahoo.com>
2025-01-23 10:23:40 -0500

    changes

--- career_center_streamlit.py
+++ career_center_streamlit.py
@@ -3,8 +3,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import first_gen
-
-
+handshake_data = pd.read_csv('handshake_data.csv')
 
 
 # Give page a title and give Uer options
@@ -13,8 +12,22 @@ topic = st.sidebar.selectbox(
     'What Do you want to look at? ',
     ['Career Center Student Engagement', 'First Gen Data', 'Internship Impact','State Retention Data', 'Career Center Surveys'])
 
-if topic = 'Career Center Student Engagement':
-    subtopic = st.selectbox('What ')
+if topic == 'Career Center Student Engagement':
+    subtopic_1= st.selectbox("Which Metric do you want to see?", ["Fairs Attendence", "Alignment", "Applications", "Career Readiness"])
+    subtopic_2 = st.selectbox("Which College Do you want to see data for ?", ["College and Graduate School of Arts & Sciences",                                                                            
+                                                                                "School of Engineering & Applied Science",           
+                                                                                "School of Architecture",                             
+                                                                                "School of Education and Human Development",          
+                                                                                "School of Data Science",                             
+                                                                                "School of Nursing",                                  
+                                                                                "School of Continuing and Professional Studies",      
+                                                                                "School of Medicine",                                 
+                                                                                "Darden Graduate School of Business Administration"])
+    visual = handshake_data.groupby([subtopic_2, 'Number of Intenrships'])[subtopic_1].agg(['mean', 'median', 'std', 'min', 'max', 
+                                                                            'count'])
+    st.write(visual)
+
+
 elif topic == 'First Gen Data':
     first_gen.user_input()
 
