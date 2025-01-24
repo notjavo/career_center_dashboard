18d9d78ef60c60d703330e25e26713154ecb1610
John Doe <lukeschneider7@yahoo.com>
2025-01-24 10:13:08 -0500

    changes

--- data_by_groups.py
+++ data_by_groups.py
@@ -6,20 +6,11 @@ import matplotlib.pyplot as plt
 handshake_data = pd.read_csv('handshake_data.csv') # Read in app Count data
 
 
-point_of_interest = 'Job Applications' # Default Metric
-school = ['College and Graduate School of Arts & Sciences', 'School of Engineering & Applied Science'] # Default School
 
 # Function to take user defined input and display data based on this input
-def user_input(subtopic_1, subtopic_2):
-    for school in subtopic_2:
-        visual = handshake_data[handshake_data['College_fds_2024'] == school].groupby([ 'College_fds_2024', 'Number of Internships',])[subtopic_1] \
-                                                                .agg(['mean', 'median', 'std', 'min', 'max','count']).round(2)
-        st.write(f" {subtopic_1} for UVA {school} 2024 graduates by Number of Internships")
-        st.write(visual)
-
-
-point_of_interest = st.selectbox("Which Metric do you want to see?", ["Internship Applications", "Job Applications", "Number of Fairs Attended", "Alignment", "Career Readiness"])
-school = st.multiselect("Which College Do you want to see data for ?", ["College and Graduate School of Arts & Sciences",                                                                            
+def user_input():
+    point_of_interest = st.selectbox("Which Metric do you want to see?", ["Internship Applications", "Job Applications", "Number of Fairs Attended", "Alignment", "Career Readiness"])
+    schools = st.multiselect("Which College Do you want to see data for ?", ["College and Graduate School of Arts & Sciences",                                                                            
                                                                                 "School of Engineering & Applied Science",           
                                                                                 "School of Architecture",                             
                                                                                 "School of Education and Human Development",          
@@ -28,5 +19,9 @@ school = st.multiselect("Which College Do you want to see data for ?", ["College
                                                                                 "School of Continuing and Professional Studies",      
                                                                                 "School of Medicine",                                 
                                                                                 "Darden Graduate School of Business Administration"])
+    for school in schools:
+        visual = handshake_data[handshake_data['College_fds_2024'] == school].groupby([ 'College_fds_2024', 'Number of Internships',])[point_of_interest] \
+                                                                .agg(['mean', 'median', 'std', 'min', 'max','count']).round(2)
+        st.write(f"{point_of_interest} for {school} 2024 graduates by Number of Internships")
+        st.write(visual)
 
-user_input(point_of_interest, school)
